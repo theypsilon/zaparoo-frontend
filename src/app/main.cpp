b4805ce6738c2ca91b7f1bcddbeff018086a8540
Callan Barrett <callan@zoocar.org>
2026-06-08 16:51:22 +0800

    Prepare v1.1 release (#209)

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -163,7 +163,7 @@ int main(int argc, char* argv[]) // NOLINT
     }
 
     QGuiApplication::setApplicationName("Zaparoo Frontend");
-    QGuiApplication::setApplicationVersion("1.0.3");
+    QGuiApplication::setApplicationVersion("1.1.0");
     QGuiApplication::setOrganizationName("Zaparoo");
     QGuiApplication::setOrganizationDomain("zaparoo.org");
 
